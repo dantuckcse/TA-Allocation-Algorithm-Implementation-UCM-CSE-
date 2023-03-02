88d7df464c6e8b471e42dfd4bafe099d9ce1e659
Soraya <60835721+sorayaboza@users.noreply.github.com>
2023-03-02 10:54:16 -0800

    Add files via upload

--- front-end/react/src/App.js
+++ front-end/react/src/App.js
@@ -1,18 +1,24 @@
 import React from "react"
 // import "./style.css"
 import AddRequests from "./components/AddRequests"
-import AddAvalable from "./components/AddAvailable"
+import AddAvailable from "./components/AddAvailable"
 import requests from "./requests"
-//import available from "./available"
+import available from "./available"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from 'react-dnd-html5-backend'
-
-import UCMLogo from "./assets/UCM-Logo.png"
 
 export default function App() {
     const user_requests = requests.map(item => {
         return (
             <AddRequests 
+                key = {item}
+                {...item}
+            />
+        )
+    })
+    const available_courses = available.map(item => {
+        return (
+            <AddAvailable 
                 key = {item}
                 {...item}
             />
@@ -35,7 +41,9 @@ export default function App() {
                 </div>
             </nav>
             <div className="drag-and-drop">
-                <div className="selected-container"></div>
+                <div className="selected-container">
+                    {available_courses}
+                </div>
                 <div className="unselected-container">
                     {/* <AddAvalable /> */}
                     <DndProvider backend={HTML5Backend}>
--- front-end/react/src/components/AddAvailable.js
+++ front-end/react/src/components/AddAvailable.js
@@ -1,10 +1,17 @@
 import React from "react"
+import { useDrag } from 'react-dnd'
 import "./comp-style.css"
 
 export default function addAvailable(prop){
     return (
-        <div>
-            <h3></h3>
+        <div className="drop-items-here">
+            <div className="courses--container">
+                <p>CSE {prop.CSE}</p>
+                <br></br>
+            </div>
+            <div className="slots--container">
+                <p>{prop.slots}</p>
+            </div>
         </div>
     )
 }

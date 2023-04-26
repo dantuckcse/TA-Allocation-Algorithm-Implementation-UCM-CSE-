c1075d2c143e2d0be15b691834fb9ee4163f1fa7
Daniel Tucker <90122022+dantuckcse@users.noreply.github.com>
2023-04-26 09:30:59 -0700

    Add files via upload

--- back-end/main_pipeline/ranking.js
+++ back-end/main_pipeline/ranking.js
@@ -1,60 +1,96 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import fs from 'fs';
-
-
-const dbPromise = open({
-
-  filename: '../database/TA_Allocation.db',
-  driver: sqlite3.Database
-});
-
+//Modifies the object formatting for front-end
 
 //Changes numbers from string to int
 //If they have the prevent or any category, then don't change
-const cleanRankings = (rows) => {
+exports.cleanRankings = (rows) => {
 
-    return rows.map((row) => {
-        
-      const courses = row.courses.split(',').map((course) => {
+  return rows.map((row) => {
 
-        if (course.includes('<span') || course.includes('ANY')) {
+    const courses = row.courses.split(',').map((course) => {
 
-          return course;
+      if (course.includes('<span') || course.includes('ANY')) {
 
-        } 
+        return course;
 
-        else {
+      }
 
-          return parseInt(course);
-        }
+      else {
 
-      });
+        return parseInt(course);
+      }
 
-      return {
-
-        ...row,
-        courses
-
-      };
     });
-  };
+
+    return {
+
+      ...row,
+      courses
+
+    };
+  });
+};
 
 
+/////////replace previous function with this one if testing using "node ranking.js"
+/*
 //Comment out to test with front-end
 const queryDatabase = async () => {
     let sql = `SELECT * FROM student_rankings;`;
-  
+
     const db = await dbPromise;
     const rows = await db.all(sql);
     const cleanRows = cleanRankings(rows);
-
-    return cleanRows;
-  
+sdvswvvsdvvssdvsdsvsdv@
+    console.log(cleanRows);
 };
 
-const app = await queryDatabase();
-fs.writeFileSync('allocation_list.json', JSON.stringify(app, null, 2));
-export default app;
+queryDatabase();
+*/
 
 
+//////////////////////////////////Another Version
+/*
+import express from 'express';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const app = express();
+
+const dbPromise = open({
+  filename: 'TA_Allocation.db',
+  driver: sqlite3.Database,
+});
+
+const cleanRankings = (rows) => {
+  return rows.map((row) => {
+    const courses = row.courses.split(',').map((course) => {
+      if (course.includes('<span')) {
+        return course;
+      } else {
+        return parseInt(course);
+      }
+    });
+    return {
+      ...row,
+      courses
+    };
+  });
+};
+
+app.get('/rankings', async (req, res, next) => {
+  try {
+    const sql = `
+      SELECT * 
+      FROM student_rankings;
+    `;
+    const db = await dbPromise;
+    const rows = await db.all(sql);
+    const cleanRows = cleanRankings(rows);
+    return res.json(cleanRows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export { app };
+*/

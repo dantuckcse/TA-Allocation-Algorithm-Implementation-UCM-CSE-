638471be5a614d0e9ac3043f23b25c0c2e40fad3
Daniel Tucker <90122022+dantuckcse@users.noreply.github.com>
2023-04-26 12:50:09 -0700

    fixed query

--- back-end/main_pipeline/display_semester.js
+++ back-end/main_pipeline/display_semester.js
@@ -28,10 +28,10 @@ exports.finalized_condition = async (_db, semesterInput) => {
 
 exports.display_allocation = async (_db, semesterInput) => {
   const sql = `
-    SELECT
+      SELECT
       A.rank AS rank,
-      (SELECT DISTINCT(A.student_name)) AS student,
-      A.pk AS id,
+      A.student_name AS student,
+      A.student_id AS id,
       (F.first_name || ' ' || F.last_name) AS professor,
       A.assigned_course AS courses
     FROM Faculty F, Assignments A, Semester S
@@ -40,8 +40,7 @@ exports.display_allocation = async (_db, semesterInput) => {
                             WHERE semester_fk = Semester.pk
                             AND term = ? AND year = ?)
       AND A.faculty_fk = F.pk
-    GROUP BY A.student_name
-    ORDER BY F.score ASC, professor;
+    ORDER BY A.rank;
   `;
 
   const args = [semesterInput.term, semesterInput.year];
